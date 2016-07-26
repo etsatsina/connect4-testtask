69bcb9204064547a78333245df594b6109d36403
etsatsina <e.tsatsina@ya.ru>
2016-07-27 01:51:14 +0200

    added mongodb dependencies and classes

---
+++ src/main/java/com/testtask/domain/GameRepository.java
@@ -0,0 +1,12 @@
+package com.testtask.domain;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Liza on 27-Jul-16.
+ */
+@Repository
+public interface GameRepository extends MongoRepository<Game, String> {
+}
